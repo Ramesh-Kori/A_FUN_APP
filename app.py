c9652037f4a928034118803ac36a40aca88997b2
Ramesh-Kori <121955072+Ramesh-Kori@users.noreply.github.com>
2023-02-25 11:52:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title(":red[Innomatics] Data App :sunglasses:")
+
+st.header("Data Science Internship")
+
+st.subheader("Feb 2023")
+
+CODE = '''print("Hello world")'''
+
+st.code(CODE, language="python")
+
+st.snow()
+
+btn_click = st.button("Click Me!")
+
+st.write(btn_click)
+
+if btn_click == True:
+    st.subheader("You clicked me :joy:")
+    st.balloons()
+    
